df9bc3128c8790dcbfc720bbc76e6cfaf3071c31
tamswebdev <portaladmin@tams.com>
2014-10-02 12:09:38 -0700

    1

--- Scripts/main.js
+++ Scripts/main.js
@@ -94,18 +94,21 @@ $( document ).on( "pagebeforeshow", "#pgSearch", function(event) {
 	
 	$('#searchCatalogs').keyup(function (event) {
 		if (event.which == 13) {
-			var _searchurl = "index.html#pgSearch?keyword=" + _encodeURIComponent($( this ).val()) + "&systemtype=" + _encodeURIComponent($("#filterDocumentType").val());
-			location.href=_searchurl;
-			location.reload(true);
+			//var _searchurl = "index.html#pgSearch?keyword=" + _encodeURIComponent($( this ).val()) + "&systemtype=" + _encodeURIComponent($("#filterDocumentType").val());
+			//location.href=_searchurl;
+			//location.reload(true);
+			
+			searchAction();
 		}
 	});
 	
 	
 	$("#filterDocumentType").change(function (event) {
-		alert($(this).val());
-		var _searchurl = "index.html#pgSearch?keyword=" + _encodeURIComponent($('#searchCatalogs').val()) + "&systemtype=" + _encodeURIComponent($(this).val());
-		location.href=_searchurl;
-		location.reload(true);
+		//alert($(this).val());
+		//var _searchurl = "index.html#pgSearch?keyword=" + _encodeURIComponent($('#searchCatalogs').val()) + "&systemtype=" + _encodeURIComponent($(this).val());
+		//location.href=_searchurl;
+		//location.reload(true);
+		searchAction();
 	});
 	
 	$("#searchCatalogs").val($.urlParam("keyword"));	
