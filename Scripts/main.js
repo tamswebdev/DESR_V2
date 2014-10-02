70459cb1432dbd7bd245eb2a4cbba875a665c1b7
tamswebdev <portaladmin@tams.com>
2014-10-02 16:11:19 -0700

    2

--- Scripts/main.js
+++ Scripts/main.js
@@ -8,14 +8,11 @@ var $scope = null;
 
 $(document).ready(function () {
 	$.mobile.pageLoadErrorMessage = "";
-	checkUserLogin();
 	
-	alert('Device Model: '    + device.model    + '<br />' +
-                            'Device Cordova: '  + device.cordova  + '<br />' +
-                            'Device Platform: ' + device.platform + '<br />' +
-                            'Device UUID: '     + device.uuid     + '<br />' +
-                            'Device Version: '  + device.version  + '<br />');
+	alert(window.device.model);
+	alert(device.model    + '<br />' + device.platform + '<br />' + device.version');
 	
+	checkUserLogin();	
 	initSystemTypes();
 });
 
