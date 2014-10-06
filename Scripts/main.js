c0b863e6f5bcf1871dcbc94bf2d06397de79a585
tamswebdev <portaladmin@tams.com>
2014-10-06 12:17:03 -0700

    1

--- Scripts/main.js
+++ Scripts/main.js
@@ -854,6 +854,8 @@ function Jsonp_Call(_url, _async, callback)
 				dataType: "jsonp",                
 				jsonpCallback: callback,
 				error: function(jqXHR, textStatus, errorThrown) {
+					alert(textStatus);
+					alert(errorThrown);
 					$("img[src='Images/loading.gif']").each(function () {
 						$(this).parent().prepend("<div class='network-unreachable' style='color: red;'>Network unreachable</div>");
 						$(this).remove();
